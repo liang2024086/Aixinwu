49ea854bec7ebd5874eb976d1236e3b3ec97299f
rye314 <rye314@sina.com>
2016-04-24 19:18:24 +0800

    modify love coin module

---
+++ AXW/app/src/main/java/com/aixinwu/axw/Adapter/ProductAdapter.java
@@ -0,0 +1,7 @@
+package com.aixinwu.axw.Adapter;
+
+/**
+ * Created by dell1 on 2016/4/24.
+ */
+public class ProductAdapter {
+}
---
+++ AXW/app/src/main/java/com/aixinwu/axw/model/Product.java
@@ -0,0 +1,7 @@
+package com.aixinwu.axw.model;
+
+/**
+ * Created by dell1 on 2016/4/24.
+ */
+public class Product {
+}
