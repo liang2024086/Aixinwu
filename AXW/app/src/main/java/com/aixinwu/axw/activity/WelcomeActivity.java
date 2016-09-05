134b57a657ac2144434a833e7a16e0fc03ae848f
liang2024086 <liangyuding@sjtu.edu.cn>
2016-09-05 20:21:26 +0800

    ww

--- AXW/app/src/main/java/com/aixinwu/axw/activity/WelcomeActivity.java
+++ AXW/app/src/main/java/com/aixinwu/axw/activity/WelcomeActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aixinwu.axw.R;
 
@@ -16,22 +17,38 @@ import com.aixinwu.axw.R;
  */
 public class WelcomeActivity extends Activity {
 
-    private final long SPLASH_LENGTH = 4000;
+    private final long SPLASH_LENGTH = 5000;
     Handler handler = new Handler();
     private TextView time;
     private MyCountDownTimer mc;
+
+    private boolean judge = true;
+
+    private myRunnable my = new myRunnable();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
 
         time = (TextView)findViewById(R.id.time);
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                judge = false;
+                //Toast.makeText(getApplicationContext(), "跳过", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
-        mc = new MyCountDownTimer(3000, 1000);
+        mc = new MyCountDownTimer(5000, 1000);
         mc.start();
 
-        handler.postDelayed(new Runnable() {  //使用handler的postDelayed实现延时跳转
+
+        handler.postDelayed(my,SPLASH_LENGTH);
+    /*    handler.postDelayed(new Runnable() {  //使用handler的postDelayed实现延时跳转
 
             public void run() {
                 Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
@@ -39,9 +56,19 @@ public class WelcomeActivity extends Activity {
                 finish();
             }
         }, SPLASH_LENGTH);//2秒后跳转至应用主界面MainActivity
-
+*/
     }
 
+    class myRunnable implements Runnable{
+
+        public void run() {
+            if (judge) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
 
     /**
      * 继承 CountDownTimer 防范
@@ -69,13 +96,13 @@ public class WelcomeActivity extends Activity {
 
         @Override
         public void onFinish() {
-            time.setText("剩余\n0秒");
+            time.setText("跳过\n0秒");
         }
 
         @Override
         public void onTick(long millisUntilFinished) {
             Log.i("MainActivity", millisUntilFinished + "");
-            time.setText("剩余\n"+millisUntilFinished / 1000 + "秒");
+            time.setText("跳过\n"+millisUntilFinished / 1000 + "秒");
         }
     }
 }
